43352214d3feee5a54b206a9927d62be78eb17c9
SwethaKopalle <kopalleswetha101@gmail.com>
2018-01-01 23:35:52 +0800

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC_003.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("webdriver.gecko.driver"	,"./Drivers/geckodriver.exe" );
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("kopella.swetha");
+		driver.findElement(By.id("pass")).sendKeys("swetha853044");
+		driver.quit();
+	}
+}
